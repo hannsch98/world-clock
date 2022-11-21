97ed421fc18e0ad2e9ba4913e23429e1fb416514
hannsch98 <101269571+hannsch98@users.noreply.github.com>
2022-11-21 13:22:18 +0100

    Select functionality to change city

--- index.js
+++ index.js
@@ -2,27 +2,52 @@ function updateTime() {
 	//Sydney
 
 	let sydneyElement = document.querySelector("#sydney");
-	let sydneyDateElement = sydneyElement.querySelector(".date");
-	let sydneyTimeElement = sydneyElement.querySelector(".time");
-	let sydneyTime = moment().tz("Australia/Sydney");
 
-	sydneyDateElement.innerHTML = sydneyTime.format("dddd[,] D[.] MMMM YYYY");
+	if (sydneyElement) {
+		let sydneyDateElement = sydneyElement.querySelector(".date");
+		let sydneyTimeElement = sydneyElement.querySelector(".time");
+		let sydneyTime = moment().tz("Australia/Sydney");
 
-	sydneyTimeElement.innerHTML = sydneyTime.format(
-		"hh:mm:ss [<small>]A[</small>]"
-	);
+		sydneyDateElement.innerHTML = sydneyTime.format("dddd[,] D[.] MMMM YYYY");
+
+		sydneyTimeElement.innerHTML = sydneyTime.format(
+			"hh:mm:ss [<small>]A[</small>]"
+		);
+	}
 
 	//Vienna
 	let viennaElement = document.querySelector("#vienna");
-	let viennaDateElement = viennaElement.querySelector(".date");
-	let viennaTimeElement = viennaElement.querySelector(".time");
-	let viennaTime = moment().tz("Europe/Vienna");
 
-	viennaDateElement.innerHTML = viennaTime.format("dddd[,] D[.] MMMM YYYY");
+	if (viennaElement) {
+		let viennaDateElement = viennaElement.querySelector(".date");
+		let viennaTimeElement = viennaElement.querySelector(".time");
+		let viennaTime = moment().tz("Europe/Vienna");
 
-	viennaTimeElement.innerHTML = viennaTime.format(
-		"hh:mm:ss [<small>]A[</small>]"
-	);
+		viennaDateElement.innerHTML = viennaTime.format("dddd[,] D[.] MMMM YYYY");
+
+		viennaTimeElement.innerHTML = viennaTime.format(
+			"hh:mm:ss [<small>]A[</small>]"
+		);
+	}
 }
 
+updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+	let cityTimezone = event.target.value;
+	let cityName = cityTimezone.replace("_", " ").split("/")[1];
+	let cityTime = moment().tz(cityTimezone);
+	let citiesElement = document.querySelector("#cities");
+	citiesElement.innerHTML = `
+        <div class="city">
+					<div>
+						<h2>${cityName}</h2>
+						<div class="date">${cityTime.format("dddd[,] D[.] MMMM YYYY")}</div>
+					</div>
+					<div class="time">${cityTime.format("hh:mm:ss [<small>]A[</small>]")}</div>
+				</div>`;
+}
+
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
